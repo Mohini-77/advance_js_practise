f889eb1f7a4a9aab80cf54f440bc5df2b76fe56e
unknown <mohinibhusare@gmail.com>
2026-01-18 15:27:20 +0530

    Fix string methods and remove splice usage

--- 14_StringMethods.js
+++ 14_StringMethods.js
@@ -1,5 +1,6 @@
-
+//All  string  Methods
 let n="mohini"
+let n1="sohini"
 
 console.log(n)                                  //mohini
 console.log(n.length)                            //6
@@ -10,9 +11,10 @@ console.log(n.concat(" is a wise"))             //mohini  is  a wise
 console.log(n.toUpperCase())                    //MOHINI
 console.log(n.toLowerCase())                    //mohini
 console.log(n.trim())                            //remove spaces mohini
-console.log(n.splice(2,5))                      // ❌ Error! strings **do not have splice** in JS 
+// console.log(n.splice(2,5))                      // ❌ Error! strings **do not have splice** in JS 
                                                 //splice is only for arrays
-
+ console.log(n1.length)                             
+console.log(n1)  
 
 
 
